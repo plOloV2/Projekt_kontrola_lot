fccf64cfe49ffec90b3459bdb82bd58fed1b6bbd
pl_OloV2 <ak.dziagwa@gmail.com>
2025-11-26 11:38:46 +0100

    lets that work begin

---
+++ include/libs.h
@@ -0,0 +1,8 @@
+#ifndef LIBS_H
+#define LIBS_H
+
+#include<stdint.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include "libs.h"
+
+int main(int argc, char** argv){
+
+
+    return 0;
+
+}
